cfa558bed227ad6ad3fd197bd32e7570a3faad8f
Oliver Rietmann <oliver.rietmann@cern.ch>
2025-04-15 15:13:16 +0200

    Install git instead of wget

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A4B469963BF863CC
 apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential libtool autoconf unzip wget libssl-dev gcc-10 g++-10 clang-18 cuda-nvcc-12-8
+DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential libtool autoconf unzip git libssl-dev gcc-10 g++-10 clang-18 cuda-nvcc-12-8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
-wget https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz
-tar -xzvf cmake-3.31.6.tar.gz
-cd cmake-3.31.6/
+git clone --depth 1 --branch v4.0.1 https://github.com/Kitware/CMake
+cd CMake 
 ./bootstrap
 make
 make install
+cd ..
+rm -rf CMake
 
